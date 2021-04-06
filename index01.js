d9d38bf3977dab941a2407f9175896ee9fce6452
ocean1229 <ocean1229@naver.com>
2021-04-06 19:11:02 +0900

    templete?engineExplain

--- index01.js
+++ index01.js
@@ -17,26 +17,24 @@ app.use((req, res, next) => {
     next();
   });
   
-  app.get('/', (req, res, next) => {
-    res.send('Welcome Home');
-  });
+  app.get('/', (req, res) => {
+    res.send('<!DOCTYPE html>\
+    <html lang="en">\
+    <head>\
+        <meta charset="UTF-8">\
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">\
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+        <title>Document</title>\
+    </head>\
+    <body>\
+        Hi. I am with html<br>\
+        <a href="/hi">Say Hi!</a>\
+    </body>\
+    </html>')
+  })
 
-
-// app.get('/goods/list', (req, res) => {
-//     res.send('상품 목록 페이지')
-//   })
-  
-//   app.get('/goods/detail', (req, res) => {
-//     res.send('상품 상세 페이지')
-//   })
-  
-//   app.get('/user/login', (req, res) => {
-//     res.send('로그인 페이지')
-//   })
-  
-//   app.get('/user/register', (req, res) => {
-//     res.send('회원가입 페이지')
-//   })
+// 템플릿 엔진 로그인 후 마이페이지 등 거의 비슷한데 조금씩만 바뀌는 경우
+// 템플릿 엔진을 쓰면 용이하다
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
