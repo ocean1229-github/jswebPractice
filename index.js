d117c42bb8d392505eff102aff2a368b7b4896c9
ocean1229 <ocean1229@naver.com>
2021-04-08 23:31:52 +0900

    week 2 homework Wrong answer

--- index.js
+++ index.js
@@ -33,6 +33,25 @@ app.get('/detail', (req, res) => {
   // 서버명
   res.render('detail');
   // 파일명
+
+  // let goods = req.query.name;
+  // res.render('detail', {goods});
+  // 이전에 내가 적은 정답인데 오류가 있었다
+  // -> 이런식으로 하는경우 ejs파일에 있는 부분까지 수정을 해주면 정상 작동되니 틀린 것은 아니다
+
+  // 선생님의 답변
+//   http://localhost:3000/detail?goods=5
+// 이렇게 보낸 url 의 ? 뒷부분을
+// querystring 이라고합니다. 올려주신대로라면 good=5 이라고 보내면 express 에서도 동일하게 req.query.good 라고 받아야 합니다
+// (두개를 같은 이름으로 지정한다면 어떤이름으로 지정해도 상관 없습니다)
+// ejs로 넘어갈때는 render 함수 안에 인자로 값을 어떻게 주느냐로 보시면 됩니다.
+// 현재 코드대로라면 {goods} 로 보내는데 그렇다면 ejs 파일 내부에서도 해당 이름으로 받도록 변경을 해줘야 합니다.
+// 음.. 그런데 숙제용 ejs 파일을 보면
+// goodsId를
+// const goodsId = urlParams.get("goodsId"); 요런식으로 가져오는데요
+// 이건 위에서 제가 말한 render 로 넘긴 값을 받아서 쓰는게 아니라 querystring 값을 바로 가져다가 쓰는 방식입니다.
+// 그래서 다시 정리해서 http://localhost:3000/detail?goods=5 이런식으로 호출하는 경우에는 ejs 파일의 저 부분을 찾으셔서 goodsId 라고 되어있는걸 goods 로 동일 하게 수정해주시면 잘 작동할 것입니다
+
 })
 
   app.get('/', (req, res) => {
